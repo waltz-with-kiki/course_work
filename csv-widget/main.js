8848224d78be6cedf0cdb89fc244a0f160f72730
wh1temeow <erasulao@gmail.com>
2023-06-03 05:40:14 +0700

    checktests

--- csv-widget/main.js
+++ csv-widget/main.js
@@ -3,31 +3,42 @@ function search() {
   var table = document.getElementById("csv-table");
   table.innerHTML = "";
 
-  
-  fetch("file.csv")
-    .then(response => response.text())
-    .then(data => {
-      var rows = data.split("\n");
-      var uniqueRows = new Set();
-      for (var i = 1; i < rows.length; i++) {
-        var cells = rows[i].split(",");
-        var rowText = cells.join(",");
-        
-        if (uniqueRows.has(rowText)) {
-          continue; 
-        }
-        uniqueRows.add(rowText);
-        for (var j = 0; j < cells.length; j++) {
-          if (cells[j].toLowerCase().indexOf(input) !== -1) {
-            var row = table.insertRow(-1);
-            for (var k = 0; k < cells.length; k++) {
-              var cell = row.insertCell(-1);
-              cell.innerHTML = cells[k];
-            }
-            break;
+  const fileInput = document.getElementById("csv-file");
+  const file = fileInput.files[0];
+
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const data = reader.result;
+    var rows = data.split("\n");
+    var uniqueRows = new Set();
+    for (var i = 1; i < rows.length; i++) {
+      var cells = rows[i].split(",");
+      var rowText = cells.join(",");
+
+      if (uniqueRows.has(rowText)) {
+        continue;
+      }
+      uniqueRows.add(rowText);
+      for (var j = 0; j < cells.length; j++) {
+        if (cells[j].toLowerCase().indexOf(input) !== -1) {
+          var row = table.insertRow(-1);
+          for (var k = 0; k < cells.length; k++) {
+            var cell = row.insertCell(-1);
+            cell.innerHTML = cells[k];
           }
+          break;
         }
       }
-    })
-    .catch(error => console.log("Ошибка загрузки CSV-файла: " + error));
+    }
+  };
+
+  reader.readAsText(file);
 }
+
+
+const fileInput = document.getElementById("csv-file");
+
+fileInput.addEventListener("change", () => {
+  search();
+});
